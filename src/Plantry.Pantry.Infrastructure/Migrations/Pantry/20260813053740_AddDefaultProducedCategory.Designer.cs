383bd37db648eca0af23ee3552bc0373692be4fe
Michael Gasparelli <magasparel@gmail.com>
2026-08-13 01:47:39 -0400

    wip: pre-flight implementation snapshot

--- src/Plantry.Pantry.Infrastructure/Migrations/Pantry/20260813053740_AddDefaultProducedCategory.Designer.cs
+++ src/Plantry.Pantry.Infrastructure/Migrations/Pantry/20260813053740_AddDefaultProducedCategory.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Plantry.Pantry.Infrastructure;
 
 #nullable disable
 
-namespace Plantry.Pantry.Infrastructure.Migrations
+namespace Plantry.Pantry.Infrastructure.Migrations.Pantry
 {
     [DbContext(typeof(PantryDbContext))]
-    partial class PantryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260813053740_AddDefaultProducedCategory")]
+    partial class AddDefaultProducedCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Plantry.Pantry.Infrastructure/Migrations/Pantry/20260813053740_AddDefaultProducedCategory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Plantry.Pantry.Infrastructure.Migrations.Pantry
+{
+    /// <inheritdoc />
+    public partial class AddDefaultProducedCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "default_produced_category_id",
+                schema: "inventory",
+                table: "household_inventory_settings",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "default_produced_category_id",
+                schema: "inventory",
+                table: "household_inventory_settings");
+        }
+    }
+}
